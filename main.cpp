2ff2b1ac9fb4ca1531f0b1689ebce23a314eb25f
Fedorov <phchen0769@icloud.com>
2022-03-03 01:34:22 +0000

    firt commit

--- main.cpp
+++ main.cpp
@@ -3,11 +3,49 @@
 
 using namespace std;
 
-int main() {
-    cout << "Hello World\n";
-    cout << "Input: ";
-    string data;
-    getline(cin, data);
-    cout << "Output: " << data << "\n\n";
+void test7(), test8(), test9(), test10(),test11();
+int main()
+{
+    // test7();
+    // test8();
+    // test9();
+    // test10();
+    test11();
     return 0;
+}
+
+void test7()
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%10d\n", a);
+    printf("%4c%6d\n", '+', b);
+    printf("-----------\n");
+    printf("%10d\n", a + b);
+}
+
+void test8()
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d\n", a * b);
+}
+
+void test9()
+{
+    int a;
+    scanf("%d", &a);
+    printf("%d\n", a * 300);
+}
+
+void test10(){
+    int a;
+    scanf("%d", &a);
+    printf("%d\n", (a+1)*100+a*10+(a+1)*2);
+}
+
+void test11(){
+    int a1,a2,a3,b1,b2,b3;
+    scanf("%d%d%d%d%d%d", &a1,&a2,&a3,&b1,&b2,&b3);
+    printf("%d\n", a1*b1+a2*b2+a3*b3);
 }
